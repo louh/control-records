bc41ca41898d4f9c4270a22e06c6aca5f2b57068
Lou Huang <lou@louhuang.com>
2021-05-18 22:00:36 -0700

    chore(deps): update all
    notes:
    - Breaking setup for vue-md-editor, despite minor semver
    - Eslint@7.x breaks with vue plugin; do not update

--- src/main.js
+++ src/main.js
@@ -8,6 +8,15 @@ import '@kangc/v-md-editor/lib/style/codemirror-editor.css'
 import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
 import enUS from '@kangc/v-md-editor/lib/lang/en-US'
 
+// Import CodeMirror
+import Codemirror from 'codemirror'
+import 'codemirror/mode/markdown/markdown'
+import 'codemirror/addon/display/placeholder'
+import 'codemirror/addon/selection/active-line'
+import 'codemirror/addon/scroll/simplescrollbars'
+import 'codemirror/addon/scroll/simplescrollbars.css'
+import 'codemirror/lib/codemirror.css'
+
 // Configure Vue
 Vue.config.productionTip = false
 
@@ -21,6 +30,7 @@ VueMarkdownEditor.lang.add({
     },
   }
 })
+VueMarkdownEditor.Codemirror = Codemirror
 VueMarkdownEditor.use(vuepressTheme)
 Vue.use(VueMarkdownEditor)
 
