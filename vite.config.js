bed80912b2faefbc826071c256e2a17fbf7920ed
Lou Huang <lou@louhuang.com>
2022-07-16 11:54:46 -0400

    chore: add useful note re: previous fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
+  // Note: vite3 compatibility fix
+  // See:
+  // https://dev.to/0xbf/vite-module-path-has-been-externalized-for-browser-compatibility-2bo6
+  // https://github.com/vitejs/vite/discussions/4479#discussioncomment-1121552
   resolve: {
     alias: {
       path: 'path-browserify'
     }
   },
-  plugins: [vue()]
 })
