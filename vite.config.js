21a2245265de09b7e1521b3cf5c0a6d6f1db6b5c
Lou Huang <lou@streetmix.net>
2025-03-18 14:37:32 -0400

    chore(vite): remove vite3 compat fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // Note: vite3 compatibility fix
-  // See:
-  // https://dev.to/0xbf/vite-module-path-has-been-externalized-for-browser-compatibility-2bo6
-  // https://github.com/vitejs/vite/discussions/4479#discussioncomment-1121552
-  resolve: {
-    alias: {
-      path: 'path-browserify'
-    }
-  },
 })
